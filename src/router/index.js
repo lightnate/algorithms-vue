a3c94760dec35f7d81761cd08b14ad9e675f8f08
light <953632095@qq.com>
2017-11-10 12:23:02 +0800

    增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Index from '@/components/Index'
 import SelectSort from '../components/SelectSort/SelectSort.vue'
 import BubbleSort from '../components/BubbleSort/BubbleSort.vue'
+import InsertSort from '../components/InsertSort/InsertSort.vue'
+import ShellSort from '../components/ShellSort/ShellSort.vue'
 
 Vue.use(Router)
 
@@ -22,6 +24,16 @@ export default new Router({
       path: '/bubble',
       name: 'BubbleSort',
       component: BubbleSort
-    }
+    },
+    {
+      path: '/insert',
+      name: 'InsertSort',
+      component: InsertSort
+    },
+    {
+      path: '/shell',
+      name: 'ShellSort',
+      component: ShellSort
+    },
   ]
 })
